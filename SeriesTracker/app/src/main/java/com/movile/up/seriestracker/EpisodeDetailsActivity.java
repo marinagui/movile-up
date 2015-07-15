f820f47c93f16df526aae33d6b1ee81313116d02
˜Marina <marinagui10@hotmail.com>
2015-07-15 13:03:58 -0300

    episodes data start

--- SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
@@ -1,12 +1,17 @@
 package com.movile.up.seriestracker;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class EpisodeDetailsActivity extends ActionBarActivity {
+
+    private static final String TAG = EpisodeDetailsActivity.class.getSimpleName();
+    private static final String episodeTitle= "asd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +19,52 @@ public class EpisodeDetailsActivity extends ActionBarActivity {
         setContentView(R.layout.episode_details_activity);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString(episodeTitle, "title episode");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        savedInstanceState.get(episodeTitle);
+        Log.d(TAG,episodeTitle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
 }
